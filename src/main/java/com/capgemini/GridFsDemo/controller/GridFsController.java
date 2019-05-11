785d245f25d64089243fa01a2ffe4cfb3b315e59
Padwankar <ruchir-shailesh.padwankar@capgemini.com>
2019-05-11 16:02:27 +0530

    add fetch() method

--- src/main/java/com/capgemini/GridFsDemo/controller/GridFsController.java
+++ src/main/java/com/capgemini/GridFsDemo/controller/GridFsController.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -16,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,38 +39,45 @@ public class GridFsController {
 	
 	String fileId="";
 	
-	@PostMapping("/upload")
-	public ResponseEntity<Video> saveFile(@RequestParam("filePath") MultipartFile file) throws IOException {
+	@PostMapping("/upload/{userName}")
+	public ResponseEntity<Video> saveFile(@RequestParam("filePath") MultipartFile file, @PathVariable String userName) throws IOException {
 		
 		if(!file.isEmpty()) {
 			System.out.println(file.getInputStream());
+			System.out.println(userName);
 			//System.out.println(userName.getName());
 		}
 		
 		//define metadata
-		//DBObject metaData = new BasicDBObject();
+		DBObject metaData = new BasicDBObject();
 		//System.out.println(video.getFileName());
 		//put organization in metadata
-		//metaData.put("organization", "Fun video");
+		metaData.put("organization", "Fun video");
+		metaData.put("userName", userName);
 		//System.out.println("fp:"+video.getFp());
-		/*
-		 * InputStream inputStream = new FileInputStream(video.getFp());
-		 * metaData.put("type", "image");
-		 */
+		
+		  InputStream inputStream = file.getInputStream();
+		  metaData.put("type", "image");
+		 
 		
 		//store image file to mongodb
-		//fileId = gridFsOperations.store(inputStream, video.getFileName(), "image/png", metaData).getId().toString();
-		return null;
-		//return new ResponseEntity<Video>(video, HttpStatus.OK);
+		fileId = gridFsOperations.store(inputStream, file.getOriginalFilename(), "image/png", metaData).getId().toString();
+		//return null;
+		return new ResponseEntity<Video>(HttpStatus.OK);
 	}
 	
-	/*
-	 * @GetMapping("/save") public String retrieveImageFile() throws IOException {
-	 * GridFSDBFile dbFile = gridFsOperations.findOne(new
-	 * Query(Criteria.where("_id").is("5cd3c37b3ff9da403c2f01b3")));
-	 * 
-	 * dbFile.writeTo("C:\\Ruchir Padwankar\\images"); return dbFile.toString();
-	 * 
-	 * }
-	 */
+	
+	  @GetMapping("/save/{username}") 
+	  public ResponseEntity<byte[]> retrieveImageFile(@PathVariable String username) throws IOException {
+		/*
+		 * GridFSDBFile dbFile = gridFsOperations.findOne(new
+		 * Query(Criteria.where("_id").is("5cd3c37b3ff9da403c2f01b3")));
+		 */
+		  GridFSDBFile dbFile = gridFsOperations.findOne(new
+				  Query(Criteria.where("metadata.userName").is(username)));
+	  System.out.println(dbFile.toString());
+	  return new ResponseEntity<byte[]>(IOUtils.toByteArray(dbFile.getInputStream()), HttpStatus.OK);
+	  
+	  }
+	 
 }
